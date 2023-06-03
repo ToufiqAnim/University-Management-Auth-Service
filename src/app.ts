86355095b7b573789b326cc4e2d42dd4d2de7150
ToufiqAnim <toufiq9610@gmail.com>
2023-06-03 06:43:56 +0600

    [DONE] improved lint setup

--- src/app.ts
+++ src/app.ts
@@ -1,13 +1,21 @@
-import express, { Application, NextFunction, Request, Response } from 'express'
+import express, { Application, Request, Response } from 'express'
 import cors from 'cors'
+import usersRouter from './app/modules/users/users.route'
 const app: Application = express()
 
 app.use(cors())
+
+//parser
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
-app.get('/', (req: Request, res: Response, next: NextFunction) => {
-  res.send('Hello World!')
-  next()
+
+// Application routes
+
+app.use('/api/v1/users/', usersRouter)
+
+//Testing
+app.get('/', async (req: Request, res: Response) => {
+  res.send('Working Successfully')
 })
 
 export default app
---
+++ src/app/modules/users/users.utils.ts
@@ -0,0 +1,41 @@
+/* import { User } from './users.model'
+
+export const findLastUserId = async () => {
+  const lastUser = await User.findOne({}, { id: 1, _id: 0 })
+    .sort({
+      createdAt: -1,
+    })
+    .lean() //lean gives document as plain old javascript objects not mongoose documents.It makes queries faster
+  return lastUser?.id
+}
+
+export const generateUserId = async () => {
+  const currentId = (await findLastUserId()) || (0).toString().padStart(5, '0')
+  const incrementId = (parseInt(currentId) + 1).toString().padStart(5, '0')
+  return incrementId
+} */
+import { User } from './users.model'
+const generateUserId = async () => {
+  //get current year
+  const currentYear = new Date().getFullYear().toString().slice(-2)
+
+  // get last user id fgorm db
+  const lastUserId = await User.findOne({}, { id: 1, _id: 0 })
+    .sort({
+      createdAt: -1,
+    })
+    .lean()
+
+  let actualId: string
+  if (lastUserId) {
+    const lastId = lastUserId.id.slice(-4)
+
+    const nextId = (parseInt(lastId) + 1).toString().padStart(4, '0')
+    actualId = nextId
+  } else {
+    actualId = '0001'
+  }
+  const id = `${currentYear}-${actualId}`
+  return id
+}
+export default generateUserId
